d26f26e5160f4faa5ea063388284fd5ee111cb1c
Muhammad Wasif Sheikh <sheikhmohammadrauf@gmail.com>
2022-09-09 19:58:32 +0500

    Ok

--- HerokuApi/settings.py
+++ HerokuApi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jbl82#mfzm67^jxz(vt#23vv+bmg&z(z-q3wp)(d%g(wi+om3b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
